e563a45876e81f305a86913250cb48ac0f16908a
LighterLukeCode <lighterluke@gmail.com>
2022-08-04 20:08:07 +0300

    start

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Мяк</h1>
+      <input placeholder="Введите логин..." />
+      <input placeholder="Введите пароль..." />
+      <button type="submit">Войти</button>
+    </>
+  );
+};
+
+export default Home;
